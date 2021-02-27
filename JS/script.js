923d7fdff7387243d2d3c3ed43e3ee246566fc68
Sergey <FlexoKub2@yandex.ru>
2021-02-28 01:14:07 +0800

    Задание №19 PopUp

--- JS/script.js
+++ JS/script.js
@@ -31,6 +31,7 @@ window.addEventListener('DOMContentLoaded', function(){
                 //вывод таймера
             function updateClock(){
                 let timer = getTimeRemaining();
+                let idInterval = setInterval(updateClock, 1000);
                 //вставляем значения
                 if(timer.hours<10) {timerHours.textContent = '0' + timer.hours;}
                 // else if (timer.hours===0) {timerHours.textContent = '00';}
@@ -44,8 +45,13 @@ window.addEventListener('DOMContentLoaded', function(){
                 if(timer.day<10) {timerDay.textContent = '0' + timer.day;}
                 // else if (timer.day===0) {timerDay.textContent = '00';}
                 else{timerDay.textContent = timer.day;}
+                if(timer.timeRemainang > 0) {
+                    
+                    setInterval(updateClock, 1000);
+                }
                 if(timer.hours<=0 && timer.minutes<=0 && timer.seconds<=0 &&
                     timer.day<=0) {
+                        clearInterval(idInterval);
                         timerHours.textContent = '00';
                         timerMinutes.textContent = '00';
                         timerSeconds.textContent = '00';
@@ -54,13 +60,75 @@ window.addEventListener('DOMContentLoaded', function(){
                             e.style.color = 'red';
                         });
                     }
-                if(timer.timeRemainang > 0) {
-                    setInterval(updateClock, 1000);
-                }
             }
             updateClock();
             
     }
-    countTimer('26 february 2021');
+    countTimer('27 february 2021');
     // setInterval(countTimer, 1000, '01 april 2021')
+//меню
+const toggleMenu = () => {
+
+    const btnMenu = document.querySelector('.menu'),
+        menu = document.querySelector('menu'),
+        closeBtn = document.querySelector('.close-btn'),
+        //список меню
+        menuItem = menu.querySelectorAll('ul>li');
+        //ф-я открытия и закрытия меню
+    const handlerMenu = () => {
+        //при помощи CSS
+        menu.classList.toggle('active-menu');
+        //     //если true при клике на меню
+        //     if(!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+        //     menu.style.transform = `translate(0)`;
+        // } else {
+        //     //закрываем меню
+        //     menu.style.transform = `translate(-100%)`;
+        // }
+    };
+
+    btnMenu.addEventListener('click', handlerMenu);
+    closeBtn.addEventListener('click', handlerMenu);
+    //цикл клик по пунктам меню
+    // for(let i=0; i<menuItem.length; i++){
+    //     menuItem[i].addEventListener('click', handlerMenu);
+    // }
+    menuItem.forEach((elem) => elem.addEventListener('click', handlerMenu));
+};
+toggleMenu();
+
+//popup
+
+const togglePopUp = () => {
+    const popup = document.querySelector('.popup'),
+        popupBtn = document.querySelectorAll('.popup-btn'),
+        popupClose = document.querySelector('.popup-close');
+       
+
+        
+        popupBtn.forEach((elem) => {
+        elem.addEventListener('click', () => {
+            
+            popup.style.display = 'block';
+            popup.style.opacity = 0;
+            requestAnimationFrame(function() {
+    popup.style.transition = 'opacity 3s ease 0.5s';
+    popup.style.opacity = 1;
+       
+    });
+        });
+        popupClose.addEventListener('click', () => {
+            // popup.style.opacity = 1;
+            requestAnimationFrame(function() {
+                popup.style.display = 'none';
+                popup.style.transition = 'opacity 3s ease 1s';
+                popup.style.opacity = 0;
+            });
+        });
+    });
+};
+togglePopUp();
+
+
+
 });
